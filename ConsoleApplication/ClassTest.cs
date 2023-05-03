bcb5ae6b48166b8ca850977a6cfe8aa46423e19e
roei6435 <Roei6435@gmail.com>
2023-05-03 17:41:01 +0300

    initial publish

---
+++ ConsoleApplication/ClassTest.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Search.Query;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class ClassTest
+    {
+
+        IOrganizationService service;
+        public OrganizationServiceContext svcContext = null;
+        public ClassTest(IOrganizationService m_service)
+        {
+            service = m_service;
+            svcContext = new OrganizationServiceContext(service);
+        }
+
+        public void GetLeadById(Guid leadId)
+        {       
+            Entity ent = service.Retrieve("lead", leadId, new ColumnSet(true));
+        }
+
+
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.Azure.ActiveDirectory.GraphClient.Internal;
+using Microsoft.Graph;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Runtime.Remoting.Contexts;
+using System.Web.Services.Description;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Started on {DateTime.Now}");
+
+            ClassTest test = new ClassTest(GetService());
+            test.GetLeadById(new Guid("6d7b103d-53c4-ed11-9886-000d3add8f0a"));
+
+
+
+
+            Console.ReadKey();  
+        }
+
+
+        public static IOrganizationService GetService()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["crm"].ConnectionString;
+            var client = new CrmServiceClient(connectionString);
+            if (client.IsReady == false)
+            {
+                throw new Exception($"Crm Service Client is not ready: {client.LastCrmError}", client.LastCrmException);
+            }
+            return client;
+        }
+         
+    }
+}
